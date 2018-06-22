2a4a2d62f7f08bdc3b606b5c93da9062e224edb5
codedken <cbsmoothoj@gmail>
2018-06-22 11:12:35 +0100

    First commit

---
+++ app/src/main/java/com/chekslate/android/chatapp/MainActivity.java
@@ -0,0 +1,34 @@
+package com.chekslate.android.chatapp;
+
+import android.annotation.SuppressLint;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("NewApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Add toolbar
+        android.support.v7.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Firebase Demo");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+}
+
+
+
+
